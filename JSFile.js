63774f3ed1d9b5c232933708ceef93c038b6158e
Bogdan5 <bogdan5x@gmail.com>
2018-07-22 14:08:54 -0400

    Comment

--- JSFile.js
+++ JSFile.js
@@ -5,34 +5,79 @@ let url2 = url1 + 'global-temperature.json';
 req.open('GET', url2, true);
 req.send();
 req.onload = () => {
-  dataJSON = JSON.parse(req.responseText);
-  graphDraw(dataJSON);
+  let monthDate;
+  let dataJSON = JSON.parse(req.responseText);
+  let newData = dataJSON.monthlyVariance.reduce((acc, item) => {
+    monthDate = new Date('1999-01-07');
+    monthDate.setMonth(item.month - 1);
+    // console.log(monthDate);
+    return acc.concat({
+      ...item,
+      month_date: monthDate,
+    });
+  }, []);
+  console.log(newData);
+  graphDraw(newData);
 };
 
 const graphDraw = (data) => {
   console.log(data);
-  let w = 1140;
-  let h = 540;
-  const padding = 100;
+  const w = 1052;
+  const h = 600;
+  const margin = { left: 70, top: 30, right: 30, bottom: 30 };
+  const minYear = d3.min(data, (d) => d.year);
+  const colorRange = ['#000066', '#0039e6', '#00ace6', '#80dfff', '#e6f9ff',
+   '#ffffb3', ' #ffcc00', '#ffbf80', '#ff7733', '#ff3300', '#990000',
+ ];
+  const rectHeight = 50;
+  const rectWidth = 4;
 
   //the x and y scales
-  let xScale = d3.scaleLinear();
-  let yScale = d3.scaleTime();
-  xScale.domain([d3.min((d) => d.year), d3.max((d) => d.year)]).range([0, w]);
-  yScale.domain([d3.min((d) => d.month), d3.max((d) => d.month)]).range([0, h]);
+  const xScale = d3.scaleLinear();
+  const yScale = d3.scaleBand();
+  xScale.domain([d3.min(data, (d) => d.year), d3.max(data, (d) => d.year)])
+    .range([0, w]);
+  yScale.domain(['January', 'February', 'March', 'April', 'May', 'June', 'July',
+  'August', 'September', 'October', 'November', 'December',
+  ])
+    .range([0, h]);
+
+  //colour scale
+  const colors = d3.scaleQuantize()
+    .domain([d3.min(data, (d) => d.variance), d3.max(data, (d) => d.variance)])
+    .range(colorRange);
+
+  const svg = d3.select('body')
+    .append('svg')
+    .attr('width', w + margin.left + margin.right)
+    .attr('height', h + margin.top + margin.bottom)
+    .attr('class', 'containerSVG');
 
   //appending the graph components
   svg.selectAll('rect')
     .data(data)
     .enter()
     .append('rect')
-    
+    .attr('x', (d) => margin.left + (d.year - minYear) * rectWidth)
+    .attr('y', (d) => margin.top + (d.month - 1) * rectHeight)
+    .attr('height', rectHeight)
+    .attr('width', rectWidth)
+    .attr('fill', (d) => colors(d.variance));
 
-  let svg = d3.select('body')
+  //axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+  svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top + h})`)
+    .call(xAxis.tickFormat(d3.format('d')));
+  svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  //legend
+  const svgLegend = d3.select('body')
     .append('svg')
-    .attr('width', w)
-    .attr('height', h)
-    .attr('class', 'containerSVG');
-
-
+    .attr('width', 100)
+    .attr('height', 50)
+    .attr('class', 'svgLegend');
 };
