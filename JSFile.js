d2752fc0a7ed6bc012d9d947616739020de851d6
Bogdan5 <bogdan5x@gmail.com>
2018-07-22 19:41:26 -0400

    Comment

--- JSFile.js
+++ JSFile.js
@@ -7,6 +7,7 @@ req.send();
 req.onload = () => {
   let monthDate;
   let dataJSON = JSON.parse(req.responseText);
+  console.log(dataJSON);
   graphDraw(dataJSON.monthlyVariance);
 };
 
@@ -14,11 +15,14 @@ const graphDraw = (data) => {
   console.log(data);
   const w = 1052;
   const h = 600;
-  const margin = { left: 100, top: 30, right: 30, bottom: 60 };
+  const margin = { left: 100, top: 30, right: 30, bottom: 100 };
   const minYear = d3.min(data, (d) => d.year);
   const colorRange = ['#000066', '#0039e6', '#00ace6', '#80dfff', '#e6f9ff',
-   '#ffffb3', ' #ffcc00', '#ffbf80', '#ff7733', '#ff3300', '#990000',
+   '#ffffb3', '#ffcc66', '#ffbf80', '#ff7733', '#ff3300', '#990000',
  ];
+ const monthsList = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+ 'August', 'September', 'October', 'November', 'December',
+  ];
   const rectHeight = 50;
   const rectWidth = 4;
 
@@ -27,9 +31,7 @@ const graphDraw = (data) => {
   const yScale = d3.scaleBand();
   xScale.domain([d3.min(data, (d) => d.year), d3.max(data, (d) => d.year)])
     .range([0, w]);
-  yScale.domain(['January', 'February', 'March', 'April', 'May', 'June', 'July',
-  'August', 'September', 'October', 'November', 'December',
-  ])
+  yScale.domain(monthsList)
     .range([0, h]);
 
   //colour scale
@@ -43,6 +45,17 @@ const graphDraw = (data) => {
     .attr('height', h + margin.top + margin.bottom)
     .attr('class', 'containerSVG');
 
+  //tooltip - a group wrapping a rect and text inside it
+  let tooltip = svg.append('g')
+    .attr('class', 'tooltip')
+    .attr('id', 'tooltip')
+    .style('visibility', 'hidden');
+
+  tooltip.append('rect')
+    .attr('class', 'rectTooltip');
+  let textTooltip = tooltip.append('text')
+    .attr('class', 'textTooltip');
+
   //appending the graph components
   svg.selectAll('rect')
     .data(data)
@@ -52,7 +65,16 @@ const graphDraw = (data) => {
     .attr('y', (d) => margin.top + (d.month - 1) * rectHeight)
     .attr('height', rectHeight)
     .attr('width', rectWidth)
-    .attr('fill', (d) => colors(d.variance));
+    .attr('fill', (d) => colors(d.variance))
+    .on('mouseover', (d) => {
+      console.log(d.year, d.month);
+      tooltip.style('visibility', 'visible');
+      textTooltip.text(`${d.year}-${monthsList[d.month - 1]}
+        ${d.variance}`);
+    })
+    .on('mouseleave', (d) => {
+      tooltip.style('visibility', 'hidden');
+    });
 
   //axes
   const xAxis = d3.axisBottom(xScale);
@@ -76,10 +98,11 @@ const graphDraw = (data) => {
   //legend
   const legHeight = 30;
   const numberColors = colorRange.length;
+  const marginsLegend = { left: 100, top: 10, };
   const svgLegend = d3.select('body')
     .append('svg')
-    .attr('width', legHeight * numberColors)
-    .attr('height', legHeight * 2)
+    .attr('width', legHeight * numberColors + marginsLegend.left)
+    .attr('height', legHeight * 2 + marginsLegend.top)
     .attr('class', 'svgLegend')
     .attr('id', 'legend');
 
@@ -88,8 +111,8 @@ const graphDraw = (data) => {
     .data(colorRange)
     .enter()
     .append('rect')
-    .attr('x', (d, i) => i * legHeight)
-    .attr('y', 0)
+    .attr('x', (d, i) => i * legHeight + marginsLegend.left)
+    .attr('y', marginsLegend.top)
     .attr('width', legHeight)
     .attr('height', legHeight)
     .style('fill', (d) => d)
@@ -97,14 +120,14 @@ const graphDraw = (data) => {
 
   //two one pixel height rects to extend the scale left and right
   svgLegend.append('rect')
-    .attr('x', 0)
-    .attr('y', legHeight)
+    .attr('x', marginsLegend.left)
+    .attr('y', legHeight + marginsLegend.top)
     .attr('height', 1)
     .attr('width', legHeight)
     .style('fill', 'black');
   svgLegend.append('rect')
-    .attr('x', legHeight * (numberColors - 1))
-    .attr('y', legHeight)
+    .attr('x', legHeight * (numberColors - 1) + marginsLegend.left)
+    .attr('y', legHeight + marginsLegend.top)
     .attr('height', 1)
     .attr('width', legHeight)
     .style('fill', 'black');
@@ -117,6 +140,7 @@ const graphDraw = (data) => {
   //axis for legendScal
   let legendAxis = d3.axisBottom(legendScale);
   svgLegend.append('g')
-    .attr('transform', `translate(${legHeight - 1}, ${legHeight})`)
+    .attr('transform', `translate(${legHeight + marginsLegend.left},
+      ${legHeight + marginsLegend.top})`)
     .call(legendAxis);
 };
