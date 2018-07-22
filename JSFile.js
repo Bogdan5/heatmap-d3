4ef524d3a66e2213dbd7da1f02dbacb39a1160a0
Bogdan5 <bogdan5x@gmail.com>
2018-07-21 22:48:09 -0400

    Comment

--- JSFile.js
+++ JSFile.js
@@ -4,11 +4,35 @@ let url1 = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/
 let url2 = url1 + 'global-temperature.json';
 req.open('GET', url2, true);
 req.send();
-req.onload(() => {
+req.onload = () => {
   dataJSON = JSON.parse(req.responseText);
-  graphDraw(jsonData);
-});
+  graphDraw(dataJSON);
+};
 
 const graphDraw = (data) => {
-  
+  console.log(data);
+  let w = 1140;
+  let h = 540;
+  const padding = 100;
+
+  //the x and y scales
+  let xScale = d3.scaleLinear();
+  let yScale = d3.scaleTime();
+  xScale.domain([d3.min((d) => d.year), d3.max((d) => d.year)]).range([0, w]);
+  yScale.domain([d3.min((d) => d.month), d3.max((d) => d.month)]).range([0, h]);
+
+  //appending the graph components
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    
+
+  let svg = d3.select('body')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+    .attr('class', 'containerSVG');
+
+
 };
